bc169565e3f84973ad720e8c4babfedbceb02bce
lcoronat <lcoronat@unal.edu.co>
2024-05-08 12:34:21 -0500

    error de js mod 8

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector("container");
+const container = document.querySelector(".container");
 
-sign_up_btn.addEventListener("click", ()  =>{
-    container.classlist.add("sign-up-moda");
+sign_up_btn.addEventListener("click", () => {
+    container.classList.add("sign-up-mode");
 });
 
 sign_in_btn.addEventListener("click", ()  =>{
-    container.classlist.remove("sign-in-moda");
+    container.classList.remove("sign-in-mode");
 });
