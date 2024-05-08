584295d68f114150b87e70f14abad4357f84719f
lcoronat <lcoronat@unal.edu.co>
2024-05-08 17:21:07 -0500

    mod 8 permanece  el error en sign in

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ sign_up_btn.addEventListener("click", () => {
     container.classList.add("sign-up-mode");
 });
 
-sign_in_btn.addEventListener("click", ()  =>{
+sign_in_btn.addEventListener("click", () => {
     container.classList.remove("sign-in-mode");
 });
